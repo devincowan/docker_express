da377ac90e7cea8ab02e1f25d620a283704b5c3e
dc <dcquickdraw@gmail.com>
2021-04-25 13:50:29 -0400

    who's the man?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()//Define port
 var port = 80//Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello Schmidtys!')
+  res.send('Hello Schmidty!')
 })//Launch listening server on port 80
 app.listen(port, function () {
   console.log('app listening...')
